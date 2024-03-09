e4297b27dd465984c8bac8e7d4ddcbea70226e4f
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-09 10:55:24 +0100

    updated Game model

--- models/Game.model.js
+++ models/Game.model.js
@@ -3,10 +3,11 @@ const { Schema, model } = mongoose;
 
 const gameSchema = new Schema({
   name: { type: String, required: true },
-  realeaseyYear: { type: String, required: true },
+  releaseYear: { type: Number, required: true },
   genre: { type: String, required: true },
+  coverArt: { type: String, required: false },
   platform: { type: String, enum: ["PSOne", "PS2", "PS3", "PS4", "PS5"] },
-  contributedBy: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
+  contributedBy: { type: Schema.Types.ObjectId, ref: 'User' },
   comments: [ { type: Schema.Types.ObjectId, ref: 'Comment' } ]
   },
   {
