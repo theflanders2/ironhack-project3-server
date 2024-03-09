df36b94717e1406e5ca0419fd7d18d8a72294a76
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-09 09:55:52 +0100

    add gameRoutes

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use("/auth", authRoutes);
 const userRoutes = require("./routes/user.routes");
 app.use("/api", isAuthenticated, userRoutes);
 
+const gameRoutes = require("./routes/game.routes");
+app.use("/api", gameRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
