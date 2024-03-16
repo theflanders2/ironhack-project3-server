aa34fad506c0ba717081d7424989e3591c73e8e0
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-16 22:27:02 +0100

    add commentRoutes

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/api", isAuthenticated, userRoutes);
 const gameRoutes = require("./routes/game.routes");
 app.use("/api", gameRoutes);
 
+const commentRoutes = require("./routes/comment.routes");
+app.use("/api", gameRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
