4b37355731f6b7edb097b3c83f57912f391d8c90
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-04-24 03:43:03 +0200

    update README and logic in seed file to add a specific userId for games added and update the gamesContributed list with the games seeded for the userId

--- README.md
+++ README.md
@@ -80,7 +80,7 @@ Game Model
   releaseYear: { type: Number, required: true },
   genre: { type: String, required: true },
   coverArtUrl: { type: String, required: false },
-  platform: { type: String, enum: ["PSOne", "PS2", "PS3", "PS4", "PS5"], required: true },
+  platform: [ { type: String, enum: ["PSOne", "PS2", "PS3", "PS4", "PS5"], required: true } ],
   contributedById: { type: Schema.Types.ObjectId, ref: 'User' },
   contributedByUser: { type: String, required: true},
   comments: [ { type: Schema.Types.ObjectId, ref: 'Comment' } ]
--- seeds/games.seed.js
+++ seeds/games.seed.js
@@ -22,7 +22,7 @@ const games = [
       releaseYear: 2018,
       genre: "Fighting",
       coverArtUrl: "https://res.cloudinary.com/dcsk4b8kl/image/upload/v1711439684/ironhack-project3/jvsgreljjabvefee8byk.jpg",
-      platform: "PS4",
+      platform: ["PS4"],
       contributedById: userId,
       contributedByUser: username,
       comments: []
@@ -31,22 +31,6 @@ const games = [
 
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ironhack-project3-server";
-
-// mongoose
-//   .connect(MONGO_URI)
-//   .then((x) => {
-//     const dbName = x.connections[0].name;
-//     console.log(`Connected to Mongo! Database name: "${dbName}"`);
-//   })
-//   .then(() => Game.create(games))
-//   .then(async (createdGames) => {
-//     const updatedUser = await User.findByIdAndUpdate(userId, {
-//       $push: { gamesContributed: { $each: createdGames.map(game => game._id) } }
-//     });
-//     console.log(`${createdGames.length} games added to db`)
-//   })
-//   .then(() => mongoose.connection.close())
-//   .catch((err) => console.log(err));
 
 async function connectAndSeed() {
   try {
