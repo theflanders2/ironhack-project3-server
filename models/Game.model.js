b9ad2d7432afb7d3709256a3f0bc285d04f428ae
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-05-07 19:11:16 +0200

    update readme and game model

--- models/Game.model.js
+++ models/Game.model.js
@@ -1,19 +1,57 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const gameSchema = new Schema({
-  name: { type: String, required: true },
-  releaseYear: { type: Number, required: true },
-  genre: { type: String, required: true },
-  coverArtUrl: { type: String, required: false },
-  platform: [ { type: String, enum: ["PSOne", "PS2", "PS3", "PS4", "PS5"], required: true } ],
-  contributedById: { type: Schema.Types.ObjectId, ref: 'User' },
-  contributedByUser: { type: String, required: true},
-  comments: [ { type: Schema.Types.ObjectId, ref: 'Comment' } ]
+const gameSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    releaseYear: { type: Number, required: true },
+    genre: [
+      {
+        type: String,
+        enum: [
+          "Action",
+          "Adventure",
+          "Arcade",
+          "Battle royale",
+          "Casual",
+          "Fighting",
+          "First-person shooter (FPS)",
+          "Horror",
+          "Massively Multiplayer Online (MMO)",
+          "Multiplayer",
+          "Multiplayer online",
+          "Multiplayer online battle arena (MOBA)",
+          "Platformer",
+          "Puzzle",
+          "Racing",
+          "Role-playing game (RPG)",
+          "Sandbox",
+          "Shooter",
+          "Simulation",
+          "Sports",
+          "Stealth",
+          "Strategy",
+          "Survival",
+          "Tactical role-playing game (RPG)",
+        ],
+        required: true,
+      },
+    ],
+    coverArtUrl: { type: String, required: false },
+    platform: [
+      {
+        type: String,
+        enum: ["PSOne", "PS2", "PS3", "PS4", "PS5"],
+        required: true,
+      },
+    ],
+    contributedById: { type: Schema.Types.ObjectId, ref: "User" },
+    contributedByUser: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
