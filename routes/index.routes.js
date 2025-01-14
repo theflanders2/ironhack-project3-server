e6439fa407cd2297214ae81ff27ee0b1296c0522
theflanders2 <kenneth.flanders.ii@gmail.com>
2025-01-14 20:20:04 +0100

    :recycle: remove dead code, implement async/await and try/catch

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,18 +2,16 @@ const router = require("express").Router();
 
 const Game = require("../models/Game.model");
 
-router.get("/", (req, res, next) => {
-    Game.find()
-      .then((allGames) => {
-        const slicePoint = allGames.length - 20;
-        const latestTenGamesAdded = allGames.slice(slicePoint).reverse();
-        res.status(200).json(latestTenGamesAdded)
-        console.log("latestTenGamesAdded", latestTenGamesAdded)
-      })
-      .catch((err) => {
-        console.log("Error while getting all games", err);
-        res.status(500).json({ message: "Error while getting all games" });
-      });
+router.get("/", async (req, res, next) => {
+  try {
+    //Retrieve the latest 10 games directly from the database
+    //If fewer than 10 games exist, return them all
+    const latestGames = await Game.find().sort({ createdAt: -1}).limit(10)
+    res.status(200).json(latestGames);
+  } catch (err) {
+    console.log("Error while fetching all games", err);
+    res.status(500).json({ message: "Error while fetching games" });
+  }
 });
 
 module.exports = router;
