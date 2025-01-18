59b3c29c2fe482c3fb6b218c70c3a71bb4c51f6a
theflanders2 <kenneth.flanders.ii@gmail.com>
2025-01-18 11:34:59 +0100

    :recycle::coffin: remove dead code, implement async/await and try/catch

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,105 +1,90 @@
 const router = require("express").Router();
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-
-// ********* require USER model in order to use it *********
 const User = require("../models/User.model");
-
-// ********* require isAuthenticated in order to use it and protect routes *********
 const { isAuthenticated } = require("../middleware/jwt.middleware")
 
 const saltRounds = 10;
 
 // POST SIGNUP PAGE /auth/signup
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
 
-    if (username === '' || password === '' || email === '') {
-        res.status(400).json({ message: "Please provide username, password and email address" });
-        return;
+    // Validate required fields
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "Please provide username, password, and email address."});
     }
-    
+
+    // Validate password strength
     const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
     if (!passwordRegex.test(password)) {
-        res.status(400).json({ message: 'Password must have at least 8 characters and contain at least one number, one lowercase and one uppercase letter' });
-        return;
+        return res.status(400).json({ message: "Password must have at least 8 characters and contain at least one number, one lowercase and one uppercase letter."});
     }
 
-    User.findOne({ email })
-        .then((foundUser) => {
-            if (foundUser) {
-            res.status(400).json({ message: "Unable to create an account with this email address. Please provide a different email address." });
-            return;
-            }
-        });
-   
-    User.findOne({ username })
-        .then((foundUser) => {
-            if (foundUser) {
-            res.status(400).json({ message: "User already exists. Please provide a different username" });
-            return;
-            }
-   
+    try {
+        const emailExists = await User.findOne({ email });
+        if (emailExists) {
+            return res.status(400).json({ message: "Unable to create an account with this email address. Please provide a different email address."})
+        }
+
+        const usernameExists = await User.findOne({ username });
+        if (usernameExists) {
+            return res.status(400).json({ message: "Unable to create an account with this username. Please provide a different username."})
+        }
+
         const salt = bcrypt.genSaltSync(saltRounds);
         const hashedPassword = bcrypt.hashSync(password, salt);
-    
-        return User.create({ username, password: hashedPassword, email });
-        })
-        .then((createdUser) => {
-            const { username, email, _id } = createdUser;
-      
-            const newUser = { username, email, _id };
 
-            res.status(201).json({ user: newUser });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ message: "Internal Server Error" })
-        });
+        const createdUser = await User.create({ username, email, password: hashedPassword });
+
+        const { username: createdUsername, email: createdEmail, _id } = createdUser;
+        const newUser = { username: createdUsername, email: createdEmail, _id };
+
+        res.status(201).json({ user: newUser });
+    }  catch(err) {
+        console.log(err);
+        return res.status(500).json({ message: "Internal server error."});
+    }
   });
 
 //POST LOGIN PAGE /auth/login
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     if (email === '' || password === '') {
-        res.status(400).json({ message: "Provide an email address and password" });
+        res.status(400).json({ message: "Provide an email address and password." });
         return;
     }
 
-    User.findOne({ email })
-        .then((foundUser) => {
-      
-            if (!foundUser) {
-            res.status(401).json({ message: "User not found" })
-            return;
-            }
-   
-            const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
-   
-            if (passwordCorrect) {
-                const { username, email, _id } = foundUser;
-          
-                const payload = { username, email, _id };
+    try {
+        const foundUser = await User.findOne({ email });
+        if (!foundUser) {
+            return res.status(401).json({ message: "User not found" })
+        }
 
-            const authToken = jwt.sign( 
-                payload,
-                process.env.TOKEN_SECRET,
-                { algorithm: 'HS256', expiresIn: "6h" }
-            );
+        const passwordCorrect = await bcrypt.compare(password, foundUser.password);
+        if (!passwordCorrect) {
+            return res.status(401).json({ message: "Unable to authenticate user" });
+        }
 
-            res.status(200).json({ authToken: authToken });
-            }
-            else {
-                res.status(401).json({ message: "Unable to authenticate user" });
-            }
-   
-        })
-        .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+        const { username, email: foundUserEmail, _id } = foundUser;
+        const payload = { username, email: foundUserEmail, _id };
+        
+        const authToken = jwt.sign(
+            payload,
+            process.env.TOKEN_SECRET,
+            { algorithm: 'HS256', expiresIn: "6h" }
+        );
+
+        res.status(200).json({ authToken });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Internal server error." })
+    }
   });
 
 // GET VERIFY PAGE /auth/verify
-router.get('/verify', isAuthenticated, (req, res, next) => {
+router.get('/verify', isAuthenticated, (req, res) => {
     res.status(200).json(req.payload);
   });
 
