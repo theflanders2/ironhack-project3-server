23347337847c9af79fe4e28bf7b1fdf86e4cde76
theflanders2 <kenneth.flanders.ii@gmail.com>
2025-01-30 17:38:45 +0100

    :recycle::coffin: remove dead code, implement async/await and try/catch

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,23 +2,20 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 
 const User = require("../models/User.model");
-const Game = require("../models/Game.model");
-const Comment = require("../models/Comment.model");
-
-const fileUploader = require("../config/cloudinary.config");
 
 // GET ALL USERS /api/users 
-router.get("/users", (req, res, next) => {
-  User.find()
-    .then((allUsers) => res.status(200).json(allUsers))
-    .catch((err) => {
-      console.log("Error getting all users", err);
-      res.status(500).json({ message: "Error getting all users" });
-    });
+router.get("/users", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.status(200).json(allUsers);
+  } catch (err) {
+    console.log("Error getting all users", err);
+    res.status(500).json({ message: "Error getting all users" });
+  }
 });
 
 // GET USER PAGE /api/users/:userId
-router.get("/users/:userId", (req, res, next) => {
+router.get("/users/:userId", async (req, res) => {
   const { userId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
@@ -26,48 +23,52 @@ router.get("/users/:userId", (req, res, next) => {
     return;
   }
 
-  User.findById(userId)
-    .populate({
-      path: "comments",
-      populate: { path: "game", select: "name" },
-    })
-    .populate("gamesContributed gamesPlayed gamesCurrentlyPlaying wishlist")
-    .then((foundUser) => {
-      const {
-        _id,
-        username,
-        email,
-        prestigeLevel,
-        avatarUrl,
-        comments,
-        gamesContributed,
-        gamesPlayed,
-        gamesCurrentlyPlaying,
-        wishlist,
-      } = foundUser;
+  try {
+    const foundUser = await User.findById(userId)
+      .populate({
+        path: "comments",
+        populate: { path: "game", select: "name" }
+      })
+      .populate("gamesContributed gamesPlayed gamesCurrentlyPlaying wishlist")
+    
+    if (!foundUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-      const reconstructedFoundUser = {
-        _id,
-        username,
-        email,
-        prestigeLevel,
-        avatarUrl,
-        comments,
-        gamesContributed,
-        gamesPlayed,
-        gamesCurrentlyPlaying,
-        wishlist,
-      };
-      res.status(200).json({ user: reconstructedFoundUser });
-    })
-    .catch((err) => {
-      console.log("Error while retrieving the user", err);
-      res.status(500).json({ message: "Error while retrieving the user" });
-    });
+    const {
+      _id,
+      username,
+      email,
+      prestigeLevel,
+      avatarUrl,
+      comments,
+      gamesContributed,
+      gamesPlayed,
+      gamesCurrentlyPlaying,
+      wishlist,
+    } = foundUser;
+    
+    const reconstructedFoundUser = {
+      _id,
+      username,
+      email,
+      prestigeLevel,
+      avatarUrl,
+      comments,
+      gamesContributed,
+      gamesPlayed,
+      gamesCurrentlyPlaying,
+      wishlist,
+    };
+    res.status(200).json({ user: reconstructedFoundUser });
+  } catch (err) {
+    console.log("Error while retrieving the user", err);
+    res.status(500).json({ message: "Error while retrieving the user" });
+  }
 });
 
 // UPDATE USER PAGE /api/users/:userId
-router.put("/users/:userId", (req, res, next) => {
+router.put("/users/:userId", async (req, res) => {
   const { userId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
@@ -75,12 +76,13 @@ router.put("/users/:userId", (req, res, next) => {
     return;
   }
 
-  User.findByIdAndUpdate(userId, req.body, { new: true })
-    .then((updatedUser) => res.json(updatedUser))
-    .catch((err) => {
-      console.log("Error while updating the user", err);
-      res.status(500).json({ message: "Error while updating the user" });
-    });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+    res.json(updatedUser);
+  } catch (err) {
+    console.log("Error while updating the user", err);
+    res.status(500).json({ message: "Error while updating the user" });
+  }
 });
 
 module.exports = router;
